457465a1cd67a5061772b1694c37e76c074934b3
Kelvin <anonymous@nowhere.geek.nz>
2023-12-03 21:30:37 +1300

    Day 3 solutions

---
+++ 03/Puzzle.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023
+
+## Day 3
+
+[2023 - Day 3](https://adventofcode.com/2023/day/3 "Advent of Code 2023 Day 3")
---
+++ 03/part2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3.12
+
+import os
+import sys
+import re
+
+
+BASEDIR = os.path.abspath(os.path.dirname(sys.argv[0]))
+DAY = os.path.basename(BASEDIR)
+
+INPUT = os.path.abspath(f"{BASEDIR}/../input/{DAY}/input.txt")
+
+lines = list()
+
+with open(INPUT, "r") as f:
+   lines=f.read().splitlines()
+
+height = len(lines)
+width = len(lines[0])
+
+part_numbers = list()
+gears = dict()
+coords = list ()
+for row, line in enumerate(lines):
+   for col, _ in enumerate(line):
+      if lines[row][col] == '*':
+         # keep co-ordinate tuples of gear symbols
+         coords.append((row, col))
+
+for i, line in enumerate(lines):
+   # find a number
+   p = 0
+   while True:
+      while p < width and line[p] not in '01234567890':
+         p = p + 1
+
+      if p == width:
+         break
+
+      m = re.match(r'(\d+)', line[p:])
+      number = int(m.group(1))
+      #print(f"Number {i}:{p} {number}", end='')
+
+      l = len(m.group(1))
+
+      for y in [ i - 1, i, i + 1 ]:
+         if y < 0 or y >= height: continue
+         for x in range(p-1, p+l+1):
+            if x < 0 or x >= width: continue
+            #print(f" {x}:{y} {lines[y][x]}", end='')
+            if (y,x) in coords:
+               loc = f"{x}:{y}"
+               if loc not in gears:
+                  gears[loc] = list()
+               gears[loc].append(number)
+
+      p = p + l
+      #print()
+
+for pn, gn in gears.items():
+   #print(f"{pn} - {gn}")
+   if len(gn) == 2:
+      part_numbers.append(gn[0] * gn[1])
+
+#print(part_numbers)
+print(sum(part_numbers))
