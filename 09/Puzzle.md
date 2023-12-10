febd82f589709da281c90dae92bbec56afae13b4
Kelvin <anonymous@nowhere.geek.nz>
2023-12-10 13:41:09 +1300

    Day 9

---
+++ 09/Puzzle.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023
+
+## Day 9
+
+[2023 - Day 9](https://adventofcode.com/2023/day/9 "Advent of Code 2023 Day 9")
---
+++ 09/part1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3.12
+
+import os
+import sys
+import re
+
+
+def next_in_pattern(p):
+   t = zip(p[:-1], p[1:])
+   s = list(map(lambda x: x[1] - x[0], t))
+
+   if sum(s) == 0:
+      s.append(0)
+      return s
+
+#   print(f"P: {p} {len(p)}")
+#   print(f"S: {s} {len(s)}")
+
+   n = next_in_pattern(s)
+#   print(f"N: {n} {len(n)}")
+
+   p.append(p[-1] + s[-1])
+
+   return p
+
+
+BASEDIR = os.path.abspath(os.path.dirname(sys.argv[0]))
+DAY = os.path.basename(BASEDIR)
+
+INPUT = os.path.abspath(f"{BASEDIR}/../input/{DAY}/input.txt")
+
+lines = list()
+
+with open(INPUT, "r") as f:
+   lines=f.read().splitlines()
+
+sequences = list()
+for line in lines:
+   sequence = list(map(int, re.split(r'\s+', line)))
+   sequences.append(sequence)
+
+nums = list()
+
+for s in sequences:
+#   print(s)
+   a = next_in_pattern(s)
+   nums.append(a[-1])
+#   print(s)
+#   print(a)
+
+#print(nums)
+print(sum(nums))
