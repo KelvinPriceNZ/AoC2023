cd3b1c4be2bf1fd931da554466af544f7888b3de
Kelvin <anonymous@nowhere.geek.nz>
2023-12-17 16:38:50 +1300

    Day 5 part 2 - needs work, takes way too long

---
+++ 05/part2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3.12
+
+import os
+import sys
+import re
+import sys
+import math
+
+
+BASEDIR = os.path.abspath(os.path.dirname(sys.argv[0]))
+DAY = os.path.basename(BASEDIR)
+
+INPUT = os.path.abspath(f"{BASEDIR}/../input/{DAY}/input.txt")
+
+lines = list()
+
+with open(INPUT, "r") as f:
+   lines=f.read().splitlines()
+
+seeds = [ int(x) for x in re.split(r'\s+', lines[0])[1:] ]
+
+link = ''
+seed_map = dict()
+
+for line in lines[2:]:
+   m = re.match(r'(\S+)\s+map:', line)
+
+   if m:
+      link = m.group(1)
+      seed_map[link] = list()
+   else:
+      nums = [ int(x) for x in re.split(r'\s+', line) if len(line) > 0 ]
+      if len(nums) == 3:
+         dst_start = nums[0]
+         src_start = nums[1]
+         range_len = nums[2]
+
+         seed_map[link].append((dst_start, src_start, range_len))
+
+lowest = math.inf
+seen = dict()
+links = seed_map.keys()
+
+while len(seeds) > 0:
+   start = seeds.pop(0)
+   seed_range = seeds.pop(0)
+   print(f"New range {start} {seed_range}", file=sys.stderr)
+   for seed in range(start, start+seed_range):
+      if seed in seen: continue
+      seen[seed] = True
+      goal = seed
+      for link in links:
+         found = False
+         map_range = None
+         for t in seed_map[link]:
+            (d, s, r) = t
+            if goal in range(s, s + r):
+               found = True
+               map_range = t
+               break
+
+         if found:
+            d, s, r = map_range
+            goal = (goal - s) + d
+
+      #print(f"{seed} {link} {goal}")
+      #print(f"!!! {seed} {link} {goal}", file=sys.stderr)
+      if goal < lowest:
+         lowest = goal
+         print(f"New lowest: {lowest}", file=sys.stderr)
+
+print(lowest)
